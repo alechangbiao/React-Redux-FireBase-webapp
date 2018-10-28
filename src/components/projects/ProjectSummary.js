0679f53f0fef689277b84a1baa993efb65f1385b
Alec Hang <alechangbiao@gmail.com>
2018-10-28 10:48:53 +0800

    adding projects (revised)

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@
         <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title ">{project.title}</span>
-          <p>Posted by Ninja H.B.</p>
+          <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
           <p className="grey-text">11th October, 10am</p>
         </div>
       </div>
